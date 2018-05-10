ad9447bb404e2ef79e354fd11f3413e95e1de70b
Samantha Rabling <sjarillorabling@gmail.com>
2018-05-09 18:19:26 -0700

    working server

--- server.js
+++ server.js
@@ -24,16 +24,36 @@ app.set("views", path.join(__dirname, "./views"));
 app.set("view engine", "ejs");
 
 // mongoose db
-mongoose.connect("mongodb://localhost/log_reg");
+mongoose.connect("mongodb://localhost/secrets");
+// comment schema
+var CommentSchema = new mongoose.Schema({
+    name: { type: String, required: true, minlength: 2 },
+    comment: { type: String, required: true, minlength: 10 }
+},
+    { timestamps: true });
+mongoose.model("Comment", CommentSchema);
+
+var Comment = mongoose.model("Comment");
+
+// secret schema
+var SecretSchema = new mongoose.Schema({
+    name: { type: String, required: true, minlength: 2 },
+    message: { type: String, required: true, minlength: 10 },
+    comment: [CommentSchema]
+},
+    { timestamps: true });
+mongoose.model("Secret", SecretSchema);
+
+var Secret = mongoose.model("Secret");
 var UserSchema = new mongoose.Schema({
     first_name: { type: String, required: true, minlenght: 2 },
     last_name: { type: String, required: true, minlenght: 2 },
     birthday: { type: Date, required: true },
     email: { type: String, unique: true, required: true, match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/] },
     password: { type: String, required: true }
+    
 });
 var User = mongoose.model("User", UserSchema);
-
 
 // routes
 app.get("/", function (req, res) {
@@ -98,7 +118,60 @@ app.post('/sessions', (req, res) => {
         }
     });
 });
+// secrets main page
+app.get("/secrets", function (req, res) {
+    Message.find({}, function (err, secrets) {
+        res.render("index", { secrets: secrets });
+    });
+});
 
+// new Message
+app.post("/newSecret", function (req, res) {
+    console.log("new message");
+    var secret = new Secret({
+        name: req.body.name,
+        secret: req.body.secret,
+    });
+    secret.save(function (err) {
+        if (err) {
+            console.log("oops! something went wrong", err);
+            for (var key in err.errors) {
+                req.flash('messages', err.errors[key].message);
+            }
+            res.redirect("/");
+        } else {
+            console.log("successfully added a secret!");
+            res.redirect("/");
+        }
+    });
+});
+
+// new Comment
+app.post("/newComment/:id", function (req, res) {
+    var secret = Secret.findOne({ _id: req.body.id }, function (err, secret) {
+        if (err) {
+            for (var key in err.errors) {
+                req.flash('comments', err.errors[key].comment);
+            }
+            res.redirect("/");
+        }
+        else {
+            secret.comment.push({ name: req.body.name, comment: req.body.comment });
+            secret.save(function (err) {
+                res.redirect("/");
+            });
+
+        }
+    });
+});  
+
+//show specific secret
+app.get("/secret/:id", function(req, res){
+    var id = req.params.id;
+    Secret.findById({ _id: id }, function (err, secrets) {
+        res.render("show", { secrets: secrets });
+    });
+});
 
 
 // port
